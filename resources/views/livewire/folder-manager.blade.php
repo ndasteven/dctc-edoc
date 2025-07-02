10bf47527919ce80b7c42121a74d41f4e8db221e
codeur2point0 <codeurdeuxpoint0@gmail.com>
2025-07-02 12:10:31 +0000

    change des icon des permission

--- resources/views/livewire/folder-manager.blade.php
+++ resources/views/livewire/folder-manager.blade.php
@@ -180,21 +180,21 @@
                         <!-- Badge de permission à gauche -->
                         <div class="flex flex-col space-y-1">
                             @if ($permission === 'L')
-                                <span>
-                                    👁️
-                                </span>
+                                <small style="font-size: 8px">
+                                    🔴
+                                </small>
                             @elseif ($permission === 'E')
-                                <span>
-                                    ✏️
-                                </span>
+                                <small style="font-size: 8px">
+                                    🟢
+                                </small>
                             @elseif ($permission === 'LE')
-                                <span>
-                                    📝
-                                </span>
+                                <small style="font-size: 8px">
+                                    🔴🟢
+                                </small>
                             @else
-                                <span>
+                                <small style="font-size: 8px">
                                     🚫
-                                </span>
+                                </small>
                             @endif
                         </div>
                         <a href="{{ route('folders.show', $folder->id) }}" class="text-blue-600 font-semibold">
@@ -295,21 +295,21 @@
                             <div class="flex justify-between items-start">
                                 <!-- Badge de permission -->
                                 @if ($filePermission === 'L')
-                                    <span>
-                                        👁️
-                                    </span>
+                                    <small style="font-size: 8px">
+                                        🔴
+                                    </small>
                                 @elseif ($filePermission === 'E')
-                                    <span>
-                                        ✏️
-                                    </span>
+                                    <small style="font-size: 8px">
+                                        🟢
+                                    </small>
                                 @elseif ($filePermission === 'LE')
-                                    <span>
-                                        📝
-                                    </span>
+                                    <small style="font-size: 8px">
+                                        🔴🟢
+                                    </small>
                                 @else
-                                    <span>
+                                    <small style="font-size: 8px">
                                         🚫
-                                    </span>
+                                    </small>
                                 @endif
                             </div>
                             <!-- Bouton menu (en haut à droite) -->
@@ -445,40 +445,7 @@
                                             </button>
                                         </small>
                                     </li> --}}
-                                    <li class="">
-                                        <small>
-                                            <button @if ($file->verrouille || $permission === 'L') disabled @endif href="#"
-                                                class="block px-4 py-1 text-white hover:bg-gray-600 hover:text-black dark:hover:bg-gray-600 dark:hover:text-white inline-flex flex justify-between items-center w-full
-            @if ($permission === 'L') opacity-50 cursor-not-allowed @endif"
-                                                @if ($permission !== 'L') @click='clickeditFile' wire:click="getFileId({{ $file->id }})" @endif>
-                                                <span>Renommer</span>
-                                                <span wire:loading wire:target='getFileId'>
-                                                    <span role="status">
-                                                        <svg aria-hidden="true"
-                                                            class="w-4 h-5 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-                                                            viewBox="0 0 100 101" fill="none"
-                                                            xmlns="http://www.w3.org/2000/svg">
-                                                            <path
-                                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                                                fill="currentColor" />
-                                                            <path
-                                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                                                fill="currentFill" />
-                                                        </svg>
-                                                    </span>
-                                                </span>
-                                                <span></span>
-                                                <svg class="w-4 h-4 text-white dark:text-white hover:text-white "
-                                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                    width="24" height="24" fill="none"
-                                                    viewBox="0 0 24 24">
-                                                    <path stroke="currentColor" stroke-linecap="round"
-                                                        stroke-linejoin="round" stroke-width="2"
-                                                        d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
-                                                </svg>
-                                            </button>
-                                        </small>
-                                    </li>
+
 
                                     <li style="border-top: solid 1px white">
                                         <button href="#"
